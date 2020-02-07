44447942e9847b277d2138b6d2f4321ca4f1590e
wepowers <60437256+wepowers@users.noreply.github.com>
2020-02-07 08:55:18 -0600

    Creating a haader

---
+++ Header.py
@@ -0,0 +1,12 @@
+# Course: CS 30
+# Period: 1
+# Date created: 20/2/7
+# Date last modified: 20/2/7
+# Name: William Powers
+# Description: Printing Name
+
+# 2-3
+first_name = "william"
+last_name = "powers"
+full_name = f"{first_name} {last_name}"
+print(f"Hello {full_name.title()}, would you like to learn some Python today?")
